ec37923c60893497b2d39952e2571cb00424fe81
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2016-05-06 02:44:14 +1000

    learning multiplication

--- test.go
+++ test.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"math/rand"
 	"github.com/NOX73/go-neural"
 	"github.com/NOX73/go-neural/learn"
 	"fmt"
 )
 
 func main() {
-	n := neural.NewNetwork(2, []int{2,1})
+	n := neural.NewNetwork(2, []int{2,3,1})
 	n.RandomizeSynapses()
 	result := n.Calculate([]float64{0.6, 0.7})
 	fmt.Printf("Result: %v\n", result)
 
-	learn.Learn(n, []float64{0, 0}, []float64{0}, 1)
-	learn.Learn(n, []float64{0.25, 0.25}, []float64{0.5}, 1)
-	learn.Learn(n, []float64{0.5, 0.5}, []float64{1}, 1)
-	learn.Learn(n, []float64{0.75, 0.75}, []float64{1.5}, 1)
-	learn.Learn(n, []float64{1, 1}, []float64{2}, 1)
+	for i := 0; i < 1000000; i++ {
+		x := rand.Float64()
+		y := rand.Float64()
+		learn.Learn(n, []float64{x, y}, []float64{x * y}, 1)
+	}
 
-	result = n.Calculate([]float64{0.6, 0.7})
-	fmt.Printf("Result after learning: %v\n", result)
+	for i := 0; i < 10; i++ {
+		x := rand.Float64()
+		y := rand.Float64()
+		fmt.Printf("%v * %v = %v\n", x, y,	n.Calculate([]float64{x, y}))
+	}
 
-	e := learn.Evaluation(n, []float64{0.6, 0.7}, []float64{10})
-	fmt.Printf("Eval: %v\n", e)
 }
