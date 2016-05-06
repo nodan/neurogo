ed46d906a1a1eb58559d5a0b8002ac55a7ce9505
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2016-05-07 03:23:12 +1000

    fixed broken code

--- gogame/groups.go
+++ gogame/groups.go
@@ -20,26 +20,23 @@ func leftOf(coords byte) (bool, byte) {
 }
 
 func above(coords byte) (bool, byte) {
-	println("above", coords)
 	if coords < n {
-		println("nothing above")
 		return false, 0
 	} else {
-		println("above returns", coords - n)
 		return true, coords - n
 	}
 }
 
 // findGroup finds the index of group of the expected side/color at the given coordinates.
 // If it is not found, it returns -1, nil
-func findGroup(grps []Group, ok bool, coords byte, side color) (bool, int, *Group) {
+func findGroup(grps []*Group, ok bool, coords byte, side color) (bool, int, *Group) {
 	fmt.Printf("findGroup %v %v %v\n", grps, coords, side)
 	if ok {
 		for i, gp := range grps {
 			r := bytes.IndexByte(gp.stoneCoords, coords)
 			if gp.side == side && r >= 0 {
 				println("found", i)
-				return true, i, &gp
+				return true, i, gp
 			}
 		}
 	}
@@ -50,18 +47,16 @@ func findGroup(grps []Group, ok bool, coords byte, side color) (bool, int, *Grou
 
 
 // categorizeGroups finds all groups
-func (g *grid) categorizeGroups() []Group {
-	grps := make([]Group, 0)
+func (g *grid) categorizeGroups() []*Group {
+	grps := make([]*Group, 0)
 	var i byte
 	newGrp := func() {
-		grps = append(grps, Group{g[i], []byte{i}})
+		grps = append(grps, &Group{g[i], []byte{i}})
 	}
 	for i = 0; i < n*n; i++ {
 		if g[i] == empty {
-			println("skip", i)
 			continue
 		}
-		println("@", i)
 		var ok bool
 		var coords byte
 		ok, coords = leftOf(i)
@@ -75,14 +70,7 @@ func (g *grid) categorizeGroups() []Group {
 			} else {
 				gp = agp
 			}
-			println(gps)
-			fmt.Printf("GP: %v\n", gp)
-			println(gp)
 			gp.stoneCoords = append(gp.stoneCoords, i)
-			fmt.Printf("GP: %v\n", gp)
-			println(gp)
-			fmt.Printf("GPS: %v\n", grps)
-			println(gps)
 		} else {
 			newGrp()
 		}
--- gogame/groups_test.go
+++ gogame/groups_test.go
@@ -9,22 +9,22 @@ func TestCategorizeGroups(t *testing.T) {
 	var g grid
 	g.mkmove(xy(1, 1), white)
 	result := g.categorizeGroups()
-	if !reflect.DeepEqual(result, []Group{Group{white, []byte{4}}}) {
+	if !reflect.DeepEqual(result, []*Group{&Group{white, []byte{4}}}) {
 		t.Errorf("Got faulty answer %v\n", result)
 	}
 
 	g.mkmove(xy(1, 2), black)
 	result = g.categorizeGroups()
-	if !reflect.DeepEqual(result, []Group{Group{white, []byte{4}},
-		Group{black, []byte{7}}}) {
+	if !reflect.DeepEqual(result, []*Group{&Group{white, []byte{4}},
+		&Group{black, []byte{7}}}) {
 		t.Errorf("Got faulty answer %v\n", result)
 	}
 
-	// g.mkmove(xy(1, 0), white).mkmove(xy(0, 2), black).mkmove(xy(2, 2), black)
-	// result = g.categorizeGroups()
-	// if !reflect.DeepEqual(result, []Group{Group{white, []byte{1, 4}},
-	//	Group{black, []byte{6, 7, 8}}}) {
-	//	t.Errorf("Got faulty answer %v\n", result)
-	// }
+	g.mkmove(xy(1, 0), white).mkmove(xy(0, 2), black).mkmove(xy(2, 2), black)
+	result = g.categorizeGroups()
+	if !reflect.DeepEqual(result, []*Group{&Group{white, []byte{1, 4}},
+		&Group{black, []byte{6, 7, 8}}}) {
+		t.Errorf("Got faulty answer %v\n", result)
+	}
 
 }
