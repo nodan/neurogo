28911cac0f675dd599b226b5cc1b4366551ebe8f
Kai Tomerius <kai@tomerius.de>
2016-05-06 16:21:36 +0200

    find neighbors

--- gogame/gogame.go
+++ gogame/gogame.go
@@ -17,10 +17,27 @@ func xy(x, y int) int {
 	return n*y+x
 }
 
-func mkmove(g *grid, xy int, c color) {
-	g[xy] = c
+func neighbors(xy int) []int {
+	rc := make([]int, 0, 4)
+	if xy%n>=1 {
+		rc = append(rc, xy-1)
+	}
+
+	if xy%n+1!=n {
+		rc = append(rc, xy+1)
+	}
+
+	if xy>=n {
+		rc = append(rc, xy-n)
+	}
+
+	if xy+n<n*n {
+		rc = append(rc, xy+n)
+	}
+
+	return rc
 }
 
-func TestMain(m *testing.M) {
-	os.Exit(m.Run())
+func mkmove(g *grid, xy int, c color) {
+	g[xy] = c
 }
