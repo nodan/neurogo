cb9f67c6784b862048d3c446951253a3cac8d8c9
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2016-05-07 22:47:03 +1000

    small refactoring

--- gonet/play.go
+++ gonet/play.go
@@ -12,19 +12,25 @@ func main() {
 	// 3 layers: inputs, processing, outputs
 	n := neural.NewNetwork(9, []int{9, 81, 9})
 	n.RandomizeSynapses()
+	lastMovePass := false
 
 	g := gogame.NewGame()
-	for !g.Finished() {
+	game: for !g.Finished() {
 		c := g.Turn()
 		s := n.Calculate(g.Board().Neural(c))
 		for {
-			xy := gogame.BestMove(s)
-			if xy<0 {
+			xy := bestMove(s)
+			if xy < 0 {
 				g.Pass()
+				if lastMovePass {
+					break game
+				}
+				lastMovePass = true
 				break
 			}
 
 			if g.Move(xy%g.Size(), xy/g.Size()) {
+				lastMovePass = false
 				break
 			}
 		}
@@ -32,11 +38,43 @@ func main() {
 
 	fmt.Println(g.ShowGame())
 
-	for _, p := g.Positions() {
-		c := g.Turn()
-		b := g.Board().Neural(c)
-		s := n.Calculate(b)
-		gogame.Demote(s, p.Played())
-		learn.Learn(n, b, s, 0.1)
+	// for _, p := g.Positions() {
+	//	c := g.Turn()
+	//	b := g.Board().Neural(c)
+	//	s := n.Calculate(b)
+	//	demote(s, p.Played())
+	//	learn.Learn(n, b, s, 0.1)
+	// }
+}
+
+
+func bestMove(s []float64) int {
+	n := gogame.Size
+	rc := -1
+	for xy := 0; xy < n*n; xy++ {
+		if s[xy]>=0 && (rc<0 || s[xy]>s[rc]) {
+			rc = xy
+		}
+	}
+
+	if rc>=0 {
+		s[rc] = -1
+	}
+	return rc
+}
+
+func demote(s []float64, xy int) {
+	n := gogame.Size
+	// find the next best move
+	nb := -1
+	for nxy := 0; nxy < n*n; nxy++ {
+		if s[nxy]>=0 && (nb<0 || s[nxy]>s[nb]) && s[nxy]<s[xy] {
+			nb = nxy
+		}
+	}
+
+	if nb>=0 {
+		// demote xy
+		s[xy] = s[nb]*0.9
 	}
 }
